d5530bfb24179565befd82b783f8e2b3349ee3b6
Gabriel Giovanini <gabrielg.desouza@gmail.com>
2019-06-16 18:16:20 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pathfinder_tools "0.1.0-SNAPSHOT"
+(defproject pathfinder_tools "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
