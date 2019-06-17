fbf26a3e6d70ec8b71902292d6f256601ea65216
Tozzi <tozzivinicius@gmail.com>
2019-06-18 00:01:17 +0200

    added function for ability modifier of player

--- src/pathfinder_tools/char_sheet.cljs
+++ src/pathfinder_tools/char_sheet.cljs
@@ -1,4 +1,14 @@
 (ns pathfinder-tools.char-sheet)
 
-(def add-weapon [char-set weapon]
-  (assoc char-set :weapons (into (:weapons char-set) add-weapon)))
+(defn add-weapon
+  [char-set weapon]
+  (assoc char-set :weapons (into (:weapons char-set) weapon)))
+
+(defn set-hp
+  [char-set hp]
+  (assoc char-set :hp hp))
+
+;;check floor function
+(defn get-ability-modifier
+  [ability-score]
+  (floor (-(/ ability-score 2)  5)))
